57afb313fe099559741315061ccb9e8976082dde
ambar.amcodr@gmail.com <ambar.amcodr@gmail.com>
2026-05-20 18:07:07 +0530

    solve some erorrs

---
+++ src/models/user.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        trim:true,
+        lowercase:true,
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Invaild email address"],
+        unique:[true, "email already exist"]
+    },
+    name:{
+        type:String,
+        required:[true, "name is required"],
+    },
+    password:{
+        type:String,
+        required:[true, "password is required"],
+        minLength:[6, "password should contain more than 6 character"],
+        select:false
+    }
+
+},{
+    timestamps: true
+});
+
+userSchema.pre("save", async function() {
+
+    if(!this.isModified("password")) {
+        return;
+    }
+
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    return;
+
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
